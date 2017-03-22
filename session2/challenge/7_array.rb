0b33f4977bd7882ee5ae8c7cabaf9e40368aa265
Alex Chalk <alexchalk17@gmail.com>
2017-03-22 16:23:30 -0400

    Completed 2:7.

--- session2/challenge/7_array.rb
+++ session2/challenge/7_array.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # Given a sentence, return an array containing every other word.
 # Punctuation is not part of the word unless it is a contraction.
 # In order to not have to write an actual language parser, there won't be any punctuation too complex.
@@ -8,3 +9,7 @@
 # alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+
+def alternate_words(sentence)
+  sentence.scan(/[A-Za-z0-9’']+/).select.with_index { |x,y| y.even? }
+end
