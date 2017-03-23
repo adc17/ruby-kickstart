305888297c4c53d54a4d3ec7dbcabf25e6c2bfcb
Alex Chalk <alexchalk17@gmail.com>
2017-03-23 15:47:46 -0400

    Completed 3:3.

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,17 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(string)
+  h = Hash.new
+  string.downcase.split.each do |x|
+    if h.has_key?(x)
+      h[x] += 1
+    else
+      h[x] = 1
+    end
+  end
+  return h
+end
+
+# Could have avoid if statement with `h = Hash.new(0)`
