3b08be3039ad4f9bfb37a34b6f88b43406ed4b29
Alex Chalk <alexchalk17@gmail.com>
2017-03-23 16:43:23 -0400

    3:5 best attempt

--- session3/challenge/5_blocks.rb
+++ session3/challenge/5_blocks.rb
@@ -16,3 +16,22 @@
 #   order << i
 # end
 # order # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+
+# Best attempt
+def spiral_access(array, &block)
+  unless array.length == 0 || array[0].length == 0
+    array[0].length.times { final_array << array[0].shift }
+    array.delete_at(0)
+    array.length.times { |i| final_array << (array[i].pop)}
+    array.last.length.times { final_array << (array.last.pop)} unless array.last.nil?
+    array.delete_at(array.length-1)
+    count = array.length -1
+    while count >= 0
+      final_array << (array[count].shift)
+      count -= 1
+    end
+    two_d(array, &block) unless array.length == 0
+  end
+  final_array.delete(nil)
+  final_array.each { |x| block.call x }
+end
