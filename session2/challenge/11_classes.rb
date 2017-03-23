cde4c976aedf4158c65f4b208e9fcb22b2aa4af0
Alex Chalk <alexchalk17@gmail.com>
2017-03-23 11:33:43 -0400

    Completed 2:11.

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -18,3 +18,64 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+class BeerSong
+  def initialize(number)
+    number = 0 if number < 0
+    number = 99 if number > 99
+    @number = number
+  end
+
+  def print_song
+    while @number >= 3
+      puts "#{@number.wordify.capitalize} bottles of beer on the wall,"
+      puts "#{@number.wordify.capitalize} bottles of beer,"
+      puts "Take one down, pass it around,"
+      @number -= 1
+      puts "#{@number.wordify.capitalize} bottles of beer on the wall."
+    end
+    if @number == 2
+      puts "#{@number.wordify.capitalize} bottles of beer on the wall,"
+      puts "#{@number.wordify.capitalize} bottles of beer,"
+      puts "Take one down, pass it around,"
+      @number -= 1
+      puts "#{@number.wordify.capitalize} bottle of beer on the wall."
+    end
+    if @number == 1
+      puts "#{@number.wordify.capitalize} bottle of beer on the wall,"
+      puts "#{@number.wordify.capitalize} bottle of beer,"
+      puts "Take one down, pass it around,"
+      @number -= 1
+      puts "#{@number.wordify.capitalize} bottles of beer on the wall."
+    end
+      
+  end
+
+end
+
+class Integer
+  
+  def wordify
+    teens_array = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+    tens_array = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    digits_array = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+    if self == 100
+      write = "one-hundred"
+    elsif self >= 20 && self <= 99
+      tens = self/10
+      digits = self - (tens * 10)
+      write = tens_array[tens]
+      write << "-" if tens > 0 && digits > 0
+      write << digits_array[digits] if digits > 0
+    elsif self >= 10 && self < 20
+      teens = self - 10
+      write = teens_array[teens]
+    elsif self >= 0 && self < 10
+      write = digits_array[self]
+    end
+    return write
+  end
+
+end
+
+BeerSong.new(99).print_song
