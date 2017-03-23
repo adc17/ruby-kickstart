0f71a7c3407236d40eededd0a30abef8eaebe8c8
Alex Chalk <alexchalk17@gmail.com>
2017-03-23 15:34:00 -0400

    Completed 3:2

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,3 +8,22 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+def staircase(integer)
+  h = Hash.new do |hash, key|
+    array = []
+    key.times do |i| 
+      if i.even? && i != 0
+        array.push(i)
+      end
+    end
+    hash[key] = array
+  end
+
+  count = 1
+  loop do
+    break if count > integer
+    h[count]
+    count += 2
+  end
+  return h
+end
